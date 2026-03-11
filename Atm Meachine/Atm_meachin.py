cee280c9925b65a12a525f51e4f7cfb6970a9351
Abdullah <abdullah240804@gmail.com>
2026-03-11 21:58:46 +0530

    Atm meachin

---
+++ Atm Meachine/Atm_meachin.py
@@ -0,0 +1,111 @@
+import random
+import json
+
+def save_data(accounts, PATH):
+    with open(PATH, "w") as file:
+        json.dump(accounts, file , indent=4)
+
+def atm():
+    PATH=r"E:\ATM MEACHIN PROJECT\Data/accounts.json.txt"
+    # Load accounts from file
+    try:
+        with open(PATH, "r") as file:
+            accounts = json.load(file)
+            accounts = {int(k): v for k, v in accounts.items()}
+    except:
+        accounts = {}
+    
+    while True:
+        
+        print("\n------ ATM MENU ------")
+        print("1. Create Account")
+        print("2. Withdraw Cash")
+        print("3. Deposit Cash")
+        print("4. Balance Enquiry")
+        print("5. Exit")
+        
+        A=int(input("Enter here: "))
+        if A==1:
+            print("Enter your details:")
+            Account_name=input("Enter your name: ")
+            age=int(input("Enter your age:"))
+            id_proof=int(input("Enter your Id Number:"))
+            account_number = random.randint(100000000000, 999999999999)
+            print(F"Your account no: {account_number}")
+            while True:
+                create_pin = input("Create 4 digit PIN: ")
+                if len(create_pin) == 4 and create_pin.isdigit():
+                    create_pin = int(create_pin)
+                    break
+                else:
+                    print("PIN must be exactly 4 digits.")
+            balance=int(input("Enter your first balance:"))
+            accounts[account_number] = {
+                "name": Account_name,
+                "age": age,
+                "pin": create_pin,
+                "balance": balance
+            }
+            # Save to file
+            save_data(accounts, PATH)
+
+            print("Account created successfully!")
+        elif A==2:
+                if not accounts:
+                    print("Please create account first.")
+                else:
+                    while True:
+                        B=input("Press 1 to continue or type exit: ").lower()
+
+                        if B=="exit":
+                            break
+                        else:
+                            account_numbers=int(input("Enter your account no: "))
+                            pin = int(input("Enter your PIN: "))
+                            if account_numbers in accounts and accounts[account_numbers]["pin"] == pin:
+                                amount=int(input("Enter amount:"))
+                                if amount <= accounts[account_numbers]["balance"]:
+                                    accounts[account_numbers]["balance"] -= amount
+                                    save_data(accounts, PATH)
+                                    print(f"Now your available balance: {accounts[account_numbers]['balance']}")
+                                else:
+                                    print("Insufficient balance in your account.")
+
+                            else:
+                                print("Incorrect PIN")
+        elif A==3:
+                if not accounts:
+                    print("Please create account first.")
+                else:
+                    while True:
+                        B=input("Press 1 to continue or type exit: ").lower()
+                        
+                    
+                        if B=="exit":
+                            break
+                        else:
+                            account_numbers=int(input("Enter your account no: "))
+                            pin = int(input("Enter your PIN: "))
+                            if account_numbers in accounts and accounts[account_numbers]["pin"] == pin:
+                                amount=int(input("Enter amount:"))
+                                accounts[account_numbers]["balance"] += amount
+                                save_data(accounts, PATH)
+                                print(f"Now available balance in your account: {accounts[account_numbers]['balance']}")
+                            else:
+                                print("Incorrect PIN")
+        elif A==4:
+                if not accounts:
+                    print("Please create account first.")
+                else:
+                    account_numbers=int(input("Enter your account no: "))
+                    pin = int(input("Enter your PIN: "))
+
+                    if account_numbers in accounts and accounts[account_numbers]["pin"] == pin:
+                        print(f"Your balance is {accounts[account_numbers]['balance']}")
+                    else:
+                        print("Incorrect PIN")
+        elif A==5:
+             break
+        else:
+             print("Invalid option.")
+atm()
